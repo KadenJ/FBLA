3a87a37d1da391745a98ad48c0b991e31d09b187
KadenJ <winners769@gmail.com>
2021-02-03 12:58:43 -0500

    new codelock fix error
    a single error rottation is outdated

---
+++ Assets/CodeLock.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeLock : MonoBehaviour
+{
+
+    int codeLength;
+    int placeInCode;
+
+    public string code = "";
+    public string attemptedCode;
+
+    public Transform toOpen;
+
+    private void Start()
+    {
+        codeLength = code.Length;
+    }
+    // Start is called before the first frame update
+    void CheckCode()
+    {
+        if(attemptedCode == code)
+        {
+            StartCoroutine(Open());
+        }
+        else
+        {
+            Debug.Log("Worng code");
+        }
+    }
+
+    IEnumerator Open()
+    {
+        //changes door rotation
+        toOpen.Rotation(new Vector3(0, 90, 0), Space.World);
+
+        yield return new WaitForSeconds(4);
+
+        toOpen.Rotate(new Vector3(0, -90, 0), Space.World);
+    }
+
+    // Update is called once per frame
+    public void SetValue(string value)
+    {
+        placeInCode++;
+
+        if(placeInCode <= codeLength)
+        {
+            attemptedCode += value;
+        }
+
+        if(placeInCode == codeLength)
+        {
+            CheckCode();
+
+            attemptedCode = "";
+            placeInCode = 0;
+        }
+    }
+}
---
+++ Assets/basees/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //calls the character controller on the player
+    public CharacterController controller;
+
+    //how fast the character would go as a float(decimal number)
+    public float speed = 12f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the input from the player and saves it as x or z (left or right for x; forward or back for z)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //uses a vector 3 to store the inputs of the player as a float by getting its position and multiplying it by x or z (Horizontal and Vertical has a default value of .001)
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //takes the character controller of the player and changes its position by multiplying the inputs stored by speed. Time.deltaTime is added to make it smooth
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+}
