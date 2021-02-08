ba7055c29cfa5c0e3de94942dbf334ef99fe9db2
KadenJ <winners769@gmail.com>
2021-02-08 12:23:24 -0500

    fix flashlight add pedestalls

---
+++ Assets/flashlightSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlightSwitch : MonoBehaviour
+{
+
+    public Light flashlight;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        }
+    }
+}
