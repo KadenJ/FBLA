8361aaf92efefaf2d03013ff03414817cbc608af
KadenJ <winners769@gmail.com>
2021-02-09 19:37:30 -0500

    finish main menu start level 3

--- Assets/keylock/Controller.cs
+++ Assets/keylock/Controller.cs
@@ -29,7 +29,12 @@ public class Controller : MonoBehaviour
         if (Physics.Raycast(ray, out hit, reachRange))
         {
             codeLock = hit.transform.gameObject.GetComponentInParent<CodeLock>();
-            GetComponent<AudioSource>().Play();
+            //GetComponent<AudioSource>().Play();
+
+            if (hit.transform.gameObject.GetComponentInParent<CodeLock>())
+            {
+                GetComponent<AudioSource>().Play();
+            }
 
             if (codeLock != null)
             {
